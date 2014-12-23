ece63a6a56b3c491295eae8dc514eb94fa250f99
The1Speck <The1Speck@users.noreply.github.com>
2014-12-22 20:45:07 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ SRVRNICK = "csgo-bleh"
 STEAMCMDPATH = "/path/to/your/steamcmd/./steamcmd.sh" #Keep the ./ as if the shell is being executed
 USER = "anonymous"
 PASS = ""
-SERVERDIR = "path/to/your/srcds_run/"
+SERVERDIR = "path/to/your/srcds_run"
 CMDLINE = "-game csgo -console -debug -usercon -maxplayers_override 24 -tickrate 64 -condebug -nobots +map de_dust2 -ip 123.456.789.11 -port 27015 +game_type 0 +game_mode 0"
 
 if screen -ls | grep -q $SRVRNICK; then
