837d9b8032c76bcbc149f53855cd3626f932994a
The1Speck <The1Speck@users.noreply.github.com>
2014-12-22 20:15:31 -0500

    Create stop.sh

---
+++ stop.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#Server Stop Script by The1Speck
+
+#VARS
+$SRVRNICK = "csgo-bleh"
+
+if screen -ls | grep -q $SRVRNICK; then
+	screen -S $SRVRNICK -X quit 
+	echo The server has been stopped.
+else
+	echo There is no server to stop.
+fi
