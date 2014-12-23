99d54a6ec473a0589ba22b5bbb2b9e0ad7916f6e
The1Speck <The1Speck@users.noreply.github.com>
2014-12-22 20:19:46 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 #Server Start Script by The1Speck
 #Vars
 SRVRNICK = "csgo-bleh"
-CMDLINE = "-game csgo -console -debug -usercon -maxplayers_override 24 -tickrate 64 -condebug -nobots +map cs_tomte -ip 123.456.789.11 -port 27015 +game_type 0 +game_mode 0"
+CMDLINE = "-game csgo -console -debug -usercon -maxplayers_override 24 -tickrate 64 -condebug -nobots +map de_dust2 -ip 123.456.789.11 -port 27015 +game_type 0 +game_mode 0"
 
 if screen -ls | grep -q $SRVRNICK; then
 	screen -S $SRVRNICK -X quit 
