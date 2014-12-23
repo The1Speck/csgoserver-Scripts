4f7edce5866fc41c13853118b7f28d0e0316d581
The1Speck <The1Speck@users.noreply.github.com>
2014-12-22 20:49:18 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ SERVERDIR="path/to/your/srcds_run"
 CMDLINE="-game csgo -console -debug -usercon -maxplayers_override 24 -tickrate 64 -condebug -nobots +map de_dust2 -ip 123.456.789.11 -port 27015 +game_type 0 +game_mode 0"
 
 if screen -ls | grep -q $SRVRNICK; then
-	screen -S c$SRVRNICK -X quit
+	screen -S $SRVRNICK -X quit
 fi
 $STEAMCMDPATH +login $USER $PASS +force_install_dir $SERVERDIR +app_update 730 +quit
 screen -dmS $SRVRNICK ./srcds_run $CMDLINE
