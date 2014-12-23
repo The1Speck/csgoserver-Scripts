8a79ce13352a93891ec14dab3c66140efc6668c7
The1Speck <The1Speck@users.noreply.github.com>
2014-12-22 20:48:17 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,12 +2,12 @@
 #Server Update Script by The1Speck
 
 #VARS
-SRVRNICK = "csgo-bleh"
-STEAMCMDPATH = "/path/to/your/steamcmd/./steamcmd.sh" #Keep the ./ as if the shell is being executed
-USER = "anonymous"
-PASS = ""
-SERVERDIR = "path/to/your/srcds_run"
-CMDLINE = "-game csgo -console -debug -usercon -maxplayers_override 24 -tickrate 64 -condebug -nobots +map de_dust2 -ip 123.456.789.11 -port 27015 +game_type 0 +game_mode 0"
+SRVRNICK="csgo-bleh"
+STEAMCMDPATH="/path/to/your/steamcmd/./steamcmd.sh" #Keep the ./ as if the shell is being executed
+USER="anonymous"
+PASS=""
+SERVERDIR="path/to/your/srcds_run"
+CMDLINE="-game csgo -console -debug -usercon -maxplayers_override 24 -tickrate 64 -condebug -nobots +map de_dust2 -ip 123.456.789.11 -port 27015 +game_type 0 +game_mode 0"
 
 if screen -ls | grep -q $SRVRNICK; then
 	screen -S c$SRVRNICK -X quit
